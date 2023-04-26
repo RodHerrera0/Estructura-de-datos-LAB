cf592455fa4f92502efec23bd0e7add4054ebe36
RodHerrera0 <70535522+RodHerrera0@users.noreply.github.com>
2023-04-26 12:50:22 -0600

    Lectura/Escritura de archivos de subastas y clientes

--- SubastaInmuebles.cs
+++ SubastaInmuebles.cs
@@ -52,4 +52,115 @@ namespace SubastaInmuebles
             this.rechazos = rechazos;
         }
 
+
+    }
+         class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lectura del archivo de clientes
+            List<Cliente> clientes = new List<Cliente>();
+            using (StreamReader archivoClientes = new StreamReader("clientes.json"))
+            {
+                string linea;
+                while ((linea = archivoClientes.ReadLine()) != null)
+                {
+                    Cliente cliente = JsonConvert.DeserializeObject<Cliente>(linea);
+                    clientes.Add(cliente);
+                }
+            }
+
+            // Creación del árbol AVL de clientes
+            AVLTree<string, Cliente> arbolClientes = new AVLTree<string, Cliente>();
+            foreach (Cliente cliente in clientes)
+            {
+                arbolClientes.Insert(cliente.dpi, cliente);
+            }
+
+            // Lectura del archivo de subastas
+            List<Subasta> subastas = new List<Subasta>();
+            using (StreamReader archivoSubastas = new StreamReader("subastas.json"))
+            {
+                string linea;
+                while ((linea = archivoSubastas.ReadLine()) != null)
+                {
+                    Subasta subasta = JsonConvert.DeserializeObject<Subasta>(linea);
+                    subastas.Add(subasta);
+                }
+            }
+            // Procesamiento de las subastas
+            foreach (Subasta subasta in subastas)
+            {
+                Console.WriteLine($"Subasta de la propiedad {subasta.propiedad}:");
+                bool aceptada = false;
+
+                foreach (Cliente cliente in subasta.clientes)
+                {
+                    Console.WriteLine($"Cliente: {cliente.firstName} {cliente.lastName}");
+
+                    // Verificación de la firma del cliente
+                    byte[] bytesFirma = System.Text.Encoding.UTF8.GetBytes($"{cliente.dpi}-{cliente.signature}");
+                    System.Security.Cryptography.SHA256Managed sha256 = new System.Security.Cryptography.SHA256Managed();
+                    byte[] hash = sha256.ComputeHash(bytesFirma);
+                    string firmaCalculada = Convert.ToBase64String(hash);
+                    if (firmaCalculada != cliente.signature)
+                    {
+                        Console.WriteLine("La firma del cliente es inválida");
+                        continue;
+                    }
+
+                    // Verificación del árbol AVL
+                    Cliente clienteAVL = arbolClientes.Search(cliente.dpi);
+                    if (clienteAVL == null)
+                    {
+                        Console.WriteLine("El cliente no está registrado en el sistema");
+                        continue;
+                    }
+
+                    // Verificación de salario mínimo
+                    if (cliente.salary < subasta.propiedad.Length * 10000)
+                    {
+                        Console.WriteLine("El salario del cliente es insuficiente para participar en la subasta");
+                        continue;
+                    }
+
+                    Console.WriteLine("El cliente es elegible para participar en la subasta");
+                    aceptada = true;
+                    break;
+                }
+
+                if (aceptada)
+                {
+                    Console.WriteLine("La subasta ha sido aceptada");
+                }
+                else
+                {
+                    subasta.rechazos++;
+                    Console.WriteLine($"La subasta ha sido rechazada, número de rechazos: {subasta.rechazos}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+    // Escritura del archivo de subastas
+    using (StreamReader archivoSubastas = new StreamReader(" input_auctions_example_lab_3.json"))
+    {
+        foreach (Subasta subasta in subastas)
+        {
+            string linea = JsonConvert.SerializeObject(subasta);
+            archivoSubastas.WriteLine(linea);
+        }
+    }
+    // Escritura del archivo de clientes
+    using (StreamReader archivoClientes = new StreamReader(" input_customer_example_lab_3.json"))
+    {
+        foreach (Cliente cliente in arbolClientes.Values)
+        {
+            string linea = JsonConvert.SerializeObject(cliente);
+            archivoClientes.WriteLine(linea);
+        }
+    }
+
+
 }
